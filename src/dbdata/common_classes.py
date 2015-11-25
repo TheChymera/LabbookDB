d20716fc9bc428087ecd79f50ffb2a95f929b2d3
Horea Christian <h.chr@mail.ru>
2015-11-25 18:57:20 +0100

    more fields for the common classes

--- src/dbdata/common_classes.py
+++ src/dbdata/common_classes.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, Column, Integer, String, Sequence, Table, ForeignKey, Float, DateTime
+from sqlalchemy import create_engine, Column, Integer, String, Sequence, Table, ForeignKey, Float, DateTime, ForeignKeyConstraint
 from sqlalchemy.orm import backref, relationship, sessionmaker
 from os import path
 
@@ -25,9 +25,11 @@ class FMRIMeasurementProtocol(Base):
 	__tablename__ = "fmri_measurement_protocols"
 	id = Column(Integer, primary_key=True)
 	scanner_setup_id = Column(Integer, ForeignKey('scanner_setups.id'))
-	scanners_setup = relationship("ScannerSetup", backref="used_for_protocols")
-	induction_anesthesia = relationship("InhalationAnesthesia", backref="used_in")
-	maintenance_anesthesia = relationship("InhalationAnesthesia", backref="used_in")
+	scanner_setup = relationship("ScannerSetup", backref="used_for_protocols")
+	induction_anesthesia_id = Column(Integer, ForeignKey('substance_administrations.id'))
+	induction_anesthesia = relationship("SubstanceAdministration", backref="used_in_protocol")
+	maintenance_anesthesia_id = Column(Integer, ForeignKey('twostep_injection_anesthesias.id'))
+	maintenance_anesthesia = relationship("TwoStepInjectionAnesthesia", backref="used_in_protocol")
 
 
 class LaserStimulationProtocol(Base):
@@ -45,9 +47,10 @@ class LaserStimulationProtocol(Base):
 class InhalationAnesthesia(Base):
 	__tablename__ = "inhalation_anesthesias"
 	id = Column(Integer, primary_key=True)
-	anesthetic = Column(String)
+	anesthetic_id = Column(Integer, ForeignKey('solutions.id'))
+	anesthetic = relationship("Solution")
 	concentration = Column(Float)
-	concentration_unit = Column(String)
+	concentration_unit = Column(String, default="%")
 	duration = Column(Float)
 	duration_unit = Column(String)
 
@@ -59,13 +62,16 @@ class ScannerSetup(Base):
 	__tablename__ = "scanner_setups"
 	id = Column(Integer, primary_key=True)
 	coil = Column(String)
-	respiraion = Column(String)
+	respiration = Column(String)
 	support = Column(String)
 
 class SubstanceAdministration(Base):
 	__tablename__ = "substance_administrations"
 	id = Column(Integer, primary_key=True)
 	date = Column(DateTime)
+	substance_id = Column(Integer, ForeignKey('solutions.id'))
+	substance = relationship("Solution", backref="used_in_inhalation")
+	route = Column(String)
 	animal_weight = Column(Float)
 	animal_weight_unit = Column(String)
 	treatment_id = Column(Integer, ForeignKey('treatments.id'))
@@ -77,14 +83,14 @@ class SubstanceAdministration(Base):
 class TwoStepInjectionAnesthesia(Base):
 	__tablename__ = "twostep_injection_anesthesias"
 	id = Column(Integer, primary_key=True)
-	anesthetic = Column(String)
-	solution_concentration = Column(Float)
-	solution_concentration_unit = Column(String)
+	anesthetic_id = Column(Integer, ForeignKey('solutions.id'))
+	anesthetic = relationship("Solution", backref="used_in_injection")
 	primary_dose = Column(Float)
-	primary_rate = Column(Float)
-	primary_rate_unit = Column(String)
+	primary_dose_unit = Column(String, default="ml/g_animal")
+	primary_rate = Column(Float, default=20)
+	primary_rate_unit = Column(String, default="mul/s")
 	secondary_rate = Column(Float)
-	secondary_rate_unit = Column(String)
+	secondary_rate_unit = Column(String, default="ml/g_animal/h")
 
 	# def __repr__(self):
 	# 	return "<Inhalation Anesthesia Step(anesthetic='%s', concentration='%s', duration='%s')>"\
@@ -100,14 +106,21 @@ class Genotype(Base):
 		return "<Genotype(name='%s', zygosity='%s')>"\
 		% (self.name, self.zygosity)
 
-class Substance(Base):
-	__tablename__ = "substances"
+class Solution(Base):
+	__tablename__ = "solutions"
 	id = Column(Integer, primary_key=True)
 	name = Column(String)
 	long_name = Column(String)
-	concentration = Column(Float)
-	concentration_unit = Column(String)
-	
+	concentration = Column(Float, default=100)
+	concentration_unit = Column(String, default="%")
+	supplier = Column(String)
+	supplier_id = Column(String)
+	contained = Column(Integer, ForeignKey("solutions.id"))
+	contains = relationship("Solution")
+
+	def __repr__(self):
+		return "<Solution(name='%s' (long_name='%s'), concentration=%s%s contains: %s)>"\
+		% (self.name, self.long_name, self.concentration, self.concentration_unit, self.contains)
 
 class Treatment(Base):
 	__tablename__ = "treatments"
