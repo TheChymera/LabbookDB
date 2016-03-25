e63314986a6aedc52328f4b66af4848b3e704a42
Horea Christian <horea.christ@yandex.com>
2016-03-25 18:19:27 +0100

    trying to query correctly

--- src/dbdata/fmri.py
+++ src/dbdata/fmri.py
@@ -2,7 +2,7 @@ import subprocess
 import tempfile
 import shutil
 from string import Template
-from sqlalchemy import create_engine, literal
+from sqlalchemy import create_engine, literal, inspection
 from sqlalchemy.orm import relation, scoped_session, sessionmaker, eagerload, subqueryload, joinedload, lazyload, Load
 from common_classes import *
 from utils import get_script_dir
@@ -38,13 +38,44 @@ def compose_instructions(table, code):
 	#!!! for a system-wide install the location should likely be redefined!
 	templates_path = os.path.join(get_script_dir(),"text_templates")
 
+	sql_query = session.query(DNAExtractionProtocol, MeasurementUnit, Incubation) \
+		.join(MeasurementUnit, MeasurementUnit.id == DNAExtractionProtocol.volume_unit_id) \
+		.join(Incubation, Incubation.id == DNAExtractionProtocol.lysis_id) \
+		.join(MeasurementUnit, MeasurementUnit.id == Incubation.temperature_unit_id) \
+		.filter(tables[table].code == code)
+
+		# .join(Incubation, Incubation.id == DNAExtractionProtocol.digestion_id) \
+	print(str(sql_query))
+
+	# cols = []
+	# joins = []
+	# insp = inspection.inspect(tables[table])
+	# for name, col in insp.columns.items():
+	# 	cols.append(col.label(name))
+	# 	# print col.label(name)
+	# # print "======"
+	# for name, rel in insp.relationships.items():
+	# 	for col_name, col in inspection.inspect(rel.mapper).columns.items():
+	# 		cols.append(col.label("{}_{}".format(name, col_name)))
+	# 		# print col.label("{}_{}".format(name, col_name))
+	# 	# print(rel.mapper.class_)
+	# 	joins.append(rel.mapper.class_)
+	# sql_query = session.query(*cols)
+	# print str(sql_query)
+	# for join in joins:
+	# 	# print str(join)
+	# 	sql_query = sql_query.join(join)
+	# sql_query = sql_query.filter(tables[table].code == code)
+	#
+
 	# get dataframe with target protocol
-	sql_query = session.query(tables[table]).options(Load(tables[table]).joinedload("*")).filter(tables[table].code == code)
-	for item in sql_query:
-		pass
-	mystring = str(sql_query)
-	mydf = pd.read_sql_query(mystring,engine,params=[code])
-	print(mydf.columns)
+	# sql_query = session.query(tables[table]).options(Load(tables[table]).joinedload("*")).filter(tables[table].code == code)
+	# for item in sql_query:
+	# 	pass
+	mystring = sql_query.statement
+	mydf = pd.read_sql_query(mystring,engine)
+	print(mydf)
+	# print(mydf.columns)
 	return
 
 	template_keys = [i.split(table+"_")[1] for i in mydf.columns.tolist()]
