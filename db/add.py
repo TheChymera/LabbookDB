5db75492e2fff57aaf546a6321b4cba304eb88b4
Horea Christian <horea.christ@yandex.com>
2016-04-11 00:07:11 +0200

    added CLI intergafec for generic add

--- db/add.py
+++ db/add.py
@@ -1,4 +1,8 @@
+#!/usr/bin/python
+
 import datetime
+import argh
+import json
 
 from sqlalchemy import create_engine, literal
 from os import path
@@ -32,7 +36,7 @@ def add_animal(db_path, id_eth, cage_eth, sex, ear_punches, id_uzh="", cage_uzh=
 
 	add_to_db(db_path, Animal(id_eth=id_eth, cage_eth=cage_eth, sex=sex, ear_punches=ear_punches, id_uzh=id_uzh, cage_uzh=cage_uzh))
 
-def add_generic(db_path, category, parameters={}):
+def add_generic(db_path, category, parameters=""):
 
 	category_class = allowed_classes[category]
 
@@ -41,6 +45,15 @@ def add_generic(db_path, category, parameters={}):
 		filtered_attributes = [i for i in attributes if i[0] != "_"]
 		message = "You can list the following keys as part of your parameters: " + ", ".join(filtered_attributes)
 		return message
+
+	parameters = json.loads(parameters)
+
+	myobject = category_class()
+	for key in parameters:
+		setattr(myobject, key, parameters[key])
+
+	add_to_db(db_path, myobject)
+
 
 def commit_and_close(session, engine):
 	session.commit()
@@ -58,6 +71,8 @@ def double_entry(db_path, field, value):
 	session.close()
 	engine.dispose()
 
+argh.dispatch_command(add_generic)
+
 if __name__ == '__main__':
-	print(add_generic("~/meta.db", "animal"))
+	add_generic("~/meta.db", "animal")
 	# add_animal("~/animal.db", 4011, 4, "f", "2L", id_uzh="M2760", cage_uzh="570971")
