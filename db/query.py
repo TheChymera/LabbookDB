e29af34aa91cc0ad148ea65ab14c5aeedc2078f1
Horea Christian <horea.christ@yandex.com>
2016-04-12 19:33:24 +0200

    added a simple query module

---
+++ db/query.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python
+
+import datetime
+import argh
+import json
+import sys
+
+import pandas as pd
+
+from sqlalchemy import create_engine, literal
+from os import path
+from common_classes import *
+from sqlalchemy.orm import sessionmaker
+import sqlalchemy
+
+allowed_classes = {
+	"animal": Animal,
+	"cage": Cage,
+	"measurementunit": MeasurementUnit,
+	"operator": Operator,
+	"substance": Substance,
+	"solution": Solution,
+	}
+
+def simple_query(db_path, category, field, value, mask="", first=True):
+	session, engine = loadSession(db_path)
+	sql_query=session.query(allowed_classes[category]).filter(getattr(allowed_classes[category], field)==value)
+	mystring = sql_query.statement
+	mydf = pd.read_sql_query(mystring,engine)
+	if mask:
+		mydf = mydf[mask]
+	if first:
+		mydf = mydf[0]
+	print(mydf)
+	session.close()
+	engine.dispose()
+
+def loadSession(db_path):
+	db_path = "sqlite:///" + path.expanduser(db_path)
+	engine = create_engine(db_path, echo=False)
+	Session = sessionmaker(bind=engine)
+	session = Session()
+	Base.metadata.create_all(engine)
+	return session, engine
+
+def commit_and_close(session, engine):
+	try:
+		session.commit()
+	except sqlalchemy.exc.IntegrityError:
+		print("Please make sure this was not a double entry.")
+	session.close()
+	engine.dispose()
+
+
+
+if __name__ == '__main__':
+	# argh.dispatch_command(add_generic)
+	simple_query("~/meta.db", "substance", "code", "flu", mask="id")
+	# add_animal("~/animal.db", 4011, 4, "f", "2L", id_uzh="M2760", cage_uzh="570971")
