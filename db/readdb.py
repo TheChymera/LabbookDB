70c363b1c2d917f2ed116fa97147b87d12314374
Horea Christian <horea.christ@yandex.com>
2016-04-14 16:13:56 +0200

    restructured demo filter code

--- db/readdb.py
+++ db/readdb.py
@@ -5,7 +5,7 @@ engine = create_engine(db_path, echo=False)
 
 import pandas as pd
 
-from common_classes import Animal, Genotype, Solution, ChronicTreatmentAdministration, ChronicTreatment, Weight, FMRIMeasurement, ChronicTreatment
+from common_classes import *
 
 from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
@@ -26,9 +26,17 @@ session = loadSession()
 	# for row1 in row.administrations:
 	# 	print row1.date
 
-# for i in session.query(Animal).filter(Animal.id_eth == 4001):
+sql_query=session.query(Incubation)
+for i in [["revolutions_per_minute",1000],["duration",60]]:
+	sql_query = sql_query.filter(getattr(Incubation, i[0])==i[1])
+# sql_query=session.query(Incubation).filter(getattr(Incubation, "revolutions_per_minute")==1000).filter(getattr(Incubation, "duration")==60)
+mystring = sql_query.statement
+mydf = pd.read_sql_query(mystring,engine)
+print(mydf)
+
+# for i in session.query(Incubation).filter(Incubation.revolutions_per_minute == 1000):
 # 	for a in i.fmri_measurements:
-# 		print a.date
+# 		print(a)
 
 # sub_query = session.query(Animal.id, ChronicTreatment)
 # sql_query = session.query(Animal.treatment).filter(ChronicTreatment.code == "chrFlu")
@@ -41,12 +49,12 @@ session = loadSession()
 # mydf = pd.read_sql_query(mystring,engine,params=["chrFlu"])
 
 
-sql_query = session.query(Animal).order_by(Animal.cage_eth)
-for item in sql_query:
-	pass
-mystring = str(sql_query)
-mydf = pd.read_sql_query(mystring,engine)
-print mydf
+# sql_query = session.query(Animal).order_by(Animal.cage_eth)
+# for item in sql_query:
+# 	pass
+# mystring = str(sql_query)
+# mydf = pd.read_sql_query(mystring,engine)
+# print(mydf)
 
 
 session.close()
