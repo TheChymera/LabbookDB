1355da87176039f35a3ee457e31dd8f71ce2f1a8
Horea Christian <horea.christ@yandex.com>
2016-09-21 01:38:50 +0200

    packaged for PIP

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+	name="LabbookDB",
+	version="",
+	description = "Database lab book standard with information addition, retrieval, and reporting functions",
+	author = "Horea Christian",
+	author_email = "horea.christ@yandex.com",
+	url = "https://github.com/TheChymera/LabbookDB",
+	keywords = ["lab book", "science", "database", "analysis", "reports", "statistics"],
+	classifiers = [],
+	install_requires = [],
+	provides = ["labbookdb"],
+	packages = ["labbookdb"],
+	)
