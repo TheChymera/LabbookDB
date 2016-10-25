ada69576ad9b8838a9a374a354ad25c765549faf
Horea Christian <horea.christ@yandex.com>
2016-10-25 05:39:50 +0200

    improved import capabilities

--- labbookdb/report/examples.py
+++ labbookdb/report/examples.py
@@ -1,4 +1,7 @@
-import selection, formatting
+try:
+	from ..report import selection, formatting
+except SystemError:
+	import selection, formatting
 if not __package__:
 	import sys, os
 	sys.path.append(os.path.expanduser('~/src/behaviopy'))
@@ -73,6 +76,6 @@ def forced_swim(db_path, plot_style, treatment_start_dates, columns=["2 to 4"], 
 		plotting.forced_swim_ttest(plottable_df, legend_loc=4, periods=periods, rename_treatments=rename_treatments)
 
 if __name__ == '__main__':
-	sucrose_prefernce(db_path, treatment_start_dates=["2016,4,25,19,30","2016,5,19,23,5"])
-	# forced_swim(db_path, "ttest", treatment_start_dates=["2016,4,25,19,30","2016,5,19,23,5"], columns=["2 to 4", "2 to 6"])
+	# sucrose_prefernce(db_path, treatment_start_dates=["2016,4,25,19,30","2016,5,19,23,5"])
+	forced_swim(db_path, "ttest", treatment_start_dates=["2016,4,25,19,30","2016,5,19,23,5"], columns=["2 to 4", "2 to 6"])
 	# forced_swim(db_path, "tsplot", treatment_start_dates=["2016,4,25,19,30","2016,5,19,23,5"])
