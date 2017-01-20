e85c498466e91d4d5137fd209dc27bfa9eeef1d1
Horea Christian <horea.christ@yandex.com>
2017-01-20 02:19:12 +0100

    removed old id columns from item representation

--- labbookdb/db/common_classes.py
+++ labbookdb/db/common_classes.py
@@ -383,7 +383,7 @@ class Animal(Base):
 	biopsies = relationship("Biopsy", backref="animal")
 
 	def __repr__(self):
-		return "<Animal(id='%s', id_eth='%s', id_uzh='%s', genotypes='%s', sex='%s', ear_punches='%s', treatment='%s')>"\
+		return "<Animal(id='%s', genotypes='%s', sex='%s', ear_punches='%s', treatment='%s')>"\
 		% (self.id, self.id_eth, self.id_uzh, [self.genotypes[i].construct+" "+self.genotypes[i].zygosity for i in range(len(self.genotypes))], self.sex, self.ear_punches,[self.treatments[i].protocol.solution for i in range(len(self.treatments))])
 
 class CageStay(Base):
