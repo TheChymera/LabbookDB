bf13a8de78dddccaef60726b591726744fec7c21
Horea Christian <horea.christ@yandex.com>
2017-01-29 20:49:15 +0100

    split classes across two modules

--- labbookdb/cli.py
+++ labbookdb/cli.py
@@ -2,12 +2,14 @@ __author__ = "Horea Christian"
 
 import argh
 
+#the order here is very important, if these modules are called in the wrong order, the declarative based will end up being regenerated and will complain about duplicates along the lines of:
+#sqlalchemy.exc.InvalidRequestError: Table 'genotype_associations' is already defined for this MetaData instance.  Specify 'extend_existing=True' to redefine options and columns on an existing Table object.
 try:
-	from report.tracking import further_cages, animal_id_table
 	from db.query import animal_info
+	from report.tracking import further_cages, animal_id_table
 except ImportError:
-	from .report.tracking import further_cages, animal_id_table
 	from .db.query import animal_info
+	from .report.tracking import further_cages, animal_id_table
 
 def main():
 	argh.dispatch_commands([animal_info, further_cages, animal_id_table])
