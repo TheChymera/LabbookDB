fdb91cd8ad1c1a37fd49d29d9fff3cd74b0db4c7
Horea Christian <horea.christ@yandex.com>
2017-01-29 19:23:45 +0100

    Python 3.4 compatible imports

--- labbookdb/cli.py
+++ labbookdb/cli.py
@@ -1,8 +1,13 @@
 __author__ = "Horea Christian"
 
 import argh
-from report.tracking import further_cages, animal_id_table
-from db.query import animal_info
+
+try:
+	from report.tracking import further_cages, animal_id_table
+	from db.query import animal_info
+except ImportError:
+	from .report.tracking import further_cages, animal_id_table
+	from .db.query import animal_info
 
 def main():
 	argh.dispatch_commands([animal_info, further_cages, animal_id_table])
