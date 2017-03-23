90e5a941aeff23cf19c8f8af782729b09473638c
Horea Christian <horea.christ@yandex.com>
2017-03-23 17:09:38 +0100

    updated sucrose preference example logic

--- labbookdb/report/examples.py
+++ labbookdb/report/examples.py
@@ -12,25 +12,28 @@ import selection
 import tracking
 import query
 
-ALL_COHORT_START_DATES = ["2016,4,25,19,30","2016,5,19,23,5","2016,11,24,21,30"]
+ALL_COHORT_START_DATES = ["2016,4,25,19,30","2016,5,19,23,5","2016,11,24,21,30","2017,1,31,22,0"]
 
-def sucrose_preference(measurement_mode):
-	if measurement_mode == "cage":
-		behaviour.sucrose_preference(db_path, treatment_start_dates=["2016,4,25,19,30","2016,5,19,23,5",])
-	elif measurement_mode == "animal":
-		behaviour.sucrose_preference(db_path, treatment_start_dates=["2016,11,24,21,30"])
-	elif measurement_mode == "aileen_switching_sides":
-		behaviour.sucrose_preference(db_path, treatment_start_dates=["2017,1,31,22,0"])
-	elif measurement_mode == "any":
-		behaviour.sucrose_preference(db_path, treatment_start_dates=ALL_COHORT_START_DATES)
-
+def sucrose_preference(cohorts, compare):
+	if cohorts == "cage":
+		treatment_start_dates = ["2016,4,25,19,30","2016,5,19,23,5",]
+	elif cohorts == "animal":
+		treatment_start_dates = ["2016,11,24,21,30"]
+	elif cohorts == "aileen_switching_sides":
+		treatment_start_dates = ["2017,1,31,22,0"]
+	elif cohorts == "all":
+		treatment_start_dates = ALL_COHORT_START_DATES
+	if compare == "treatment":
+		behaviour.sucrose_preference(db_path, treatment_start_dates=treatment_start_dates, comparisons={"Period [days]":[]}, compare="Treatment",)
+	elif compare == "side_preference":
+		behaviour.sucrose_preference(db_path, treatment_start_dates=treatment_start_dates, comparisons={"Cage ID":[]}, compare="Sucrose Bottle Position",)
 
 if __name__ == '__main__':
 	db_path="~/syncdata/meta.db"
 	# forced_swim(db_path, "pointplot", treatment_start_dates=["2016,4,25,19,30","2016,5,19,23,5"])
 	# forced_swim(db_path, "pointplot", treatment_start_dates=["2016,4,25,19,30","2016,5,19,23,5"], period_label="interval [2 min]")
 
-	sucrose_preference("aileen")
+	sucrose_preference("aileen_switching_sides", "treatment")
 
 	# behaviour.forced_swim(db_path, "tsplot", treatment_start_dates=["2016,4,25,19,30","2016,5,19,23,5"])
 	# behaviour.forced_swim(db_path, "tsplot", treatment_start_dates=["2016,11,24,21,30"])
