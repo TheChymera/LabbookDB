bcf00ec46dc656a53041d24781ffe22eccbe8f37
Horea Christian <horea.christ@yandex.com>
2017-04-20 03:33:39 +0200

    extended selection capabilities to allow better timetable and animal query

--- labbookdb/report/selection.py
+++ labbookdb/report/selection.py
@@ -60,6 +60,58 @@ def animals_by_cage_treatment(db_path,
 
 	return df
 
+def animals_by_treatment(db_path,
+	codes=[],
+	end_dates=[],
+	start_dates=[],
+	):
+	"""Select a dataframe of animals and all related tables through to cage treatments based on cage_treatment filters.
+
+	Parameters
+	----------
+
+	db_path : string
+	Path to a LabbookDB formatted database.
+
+	codes : list, optional
+	Related TreatmentProtocol.code values based on which to filter dataframe
+
+	end_dates : list, optional
+	Related Treatment.end_date values based on which to filter dataframe
+
+	start_dates : list, optional
+	Related Treatment.start_date values based on which to filter dataframe
+	"""
+
+	col_entries=[
+		("Animal","id"),
+		("Treatment",),
+		("TreatmentProtocol","code"),
+		]
+	join_entries=[
+		("Animal.treatments",),
+		("Treatment.protocol",),
+		]
+	my_filters=[]
+	if codes:
+		my_filter = ["TreatmentProtocol","code"]
+		my_filter.extend(codes)
+		my_filters.append(my_filter)
+	if end_dates:
+		my_filter = ["Treatment","end_date"]
+		my_filter.extend(end_dates)
+		my_filters.append(my_filter)
+	if start_dates:
+		my_filter = ["Treatment","start_date"]
+		my_filter.extend(start_dates)
+		my_filters.append(my_filter)
+	if not my_filters:
+		my_filters=[None]
+
+	df = query.get_df(db_path,col_entries=col_entries, join_entries=join_entries, filters=my_filters)
+
+	return df
+
 def by_animals(db_path, select, animals):
 	"""Select a dataframe of animals and all related tables through to the "select" table based on animal filters.
 
@@ -76,7 +128,7 @@ def by_animals(db_path, select, animals):
 	Animal.id values based on which to filter dataframe
 	"""
 
-	accepted_select_values = ["sucrose preference"]
+	accepted_select_values = ["sucrose preference","timetable"]
 
 	if select == "sucrose preference":
 		join_types = ["inner","inner","inner"]
@@ -89,6 +141,31 @@ def by_animals(db_path, select, animals):
 			("Animal.cage_stays",),
 			("CageStay.cage",),
 			("SucrosePreferenceMeasurement",),
+			]
+	elif select == "timetable":
+		col_entries=[
+			("Animal","id"),
+			("Treatment",),
+			("FMRIMeasurement","date"),
+			("OpenFieldTestMeasurement","date"),
+			("ForcedSwimTestMeasurement","date"),
+			("TreatmentProtocol","code"),
+			("Cage","id"),
+			("Cage","Treatment",""),
+			("Cage","TreatmentProtocol","code"),
+			("SucrosePreferenceMeasurement","date"),
+			]
+		join_entries=[
+			("Animal.treatments",),
+			("FMRIMeasurement",),
+			("OpenFieldTestMeasurement","Animal.measurements"),
+			("ForcedSwimTestMeasurement","Animal.measurements"),
+			("Treatment.protocol",),
+			("Animal.cage_stays",),
+			("CageStay.cage",),
+			("Cage_Treatment","Cage.treatments"),
+			("Cage_TreatmentProtocol","Cage_Treatment.protocol"),
+			("SucrosePreferenceMeasurement","Cage.measurements"),
 			]
 	else:
 		raise ValueError("The value for select needs to be one of {}".format(accepted_select_values))
