be4c461153d5818706109ecf562b819a02db4abc
Horea Christian <horea.christ@yandex.com>
2017-05-24 03:37:55 +0200

    no longer executing setup in main

--- setup.py
+++ setup.py
@@ -1,21 +1,22 @@
 from setuptools import setup
 
-setup(
-	name="LabbookDB",
-	version="",
-	description = "Database lab book standard with information addition, retrieval, and reporting functions",
-	author = "Horea Christian",
-	author_email = "horea.christ@yandex.com",
-	url = "https://github.com/TheChymera/LabbookDB",
-	keywords = ["lab book", "science", "database", "analysis", "reports", "statistics"],
-	classifiers = [],
-	install_requires = [],
-	provides = ["labbookdb"],
-	packages = ["labbookdb",
-		"labbookdb.db",
-		"labbookdb.report",
-		],
-	entry_points = {'console_scripts' : \
-			['LDB = labbookdb.cli:main']
-		}
-	)
+if __name__ == '__main__':
+	setup(
+		name="LabbookDB",
+		version="",
+		description = "Database lab book standard with information addition, retrieval, and reporting functions",
+		author = "Horea Christian",
+		author_email = "horea.christ@yandex.com",
+		url = "https://github.com/TheChymera/LabbookDB",
+		keywords = ["lab book", "science", "database", "analysis", "reports", "statistics"],
+		classifiers = [],
+		install_requires = [],
+		provides = ["labbookdb"],
+		packages = ["labbookdb",
+			"labbookdb.db",
+			"labbookdb.report",
+			],
+		entry_points = {'console_scripts' : \
+				['LDB = labbookdb.cli:main']
+			}
+		)
