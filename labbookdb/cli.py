c0cd91f573573495ded984a16506a626db67b0d5
Horea Christian <horea.christ@yandex.com>
2017-05-29 05:18:10 +0200

    added functions and improved imports

--- labbookdb/cli.py
+++ labbookdb/cli.py
@@ -4,15 +4,20 @@ import argh
 
 #the order here is very important, if these modules are called in the wrong order, the declarative base will end up being regenerated and will complain about duplicates along the lines of:
 #sqlalchemy.exc.InvalidRequestError: Table 'genotype_associations' is already defined for this MetaData instance.  Specify 'extend_existing=True' to redefine options and columns on an existing Table object.
-try:
-	from db.query import animal_info, cage_info
-	from report.tracking import further_cages, animals_id, animals_info
-except ImportError:
-	from .db.query import animal_info, cage_info
-	from .report.tracking import further_cages, animals_id, animals_info
+from .db.add import add_generic, append_parameter
+from .db.query import animal_info, cage_info
+from .report.tracking import further_cages, animals_id, animals_info
 
 def main():
-	argh.dispatch_commands([animal_info, cage_info, further_cages, animals_id, animals_info])
+	argh.dispatch_commands([
+		add_generic,
+		animal_info,
+		animals_id,
+		animals_info,
+		append_parameter,
+		cage_info,
+		further_cages,
+		])
 
 if __name__ == '__main__':
 	main()
