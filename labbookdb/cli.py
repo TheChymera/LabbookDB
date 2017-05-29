6691be073f7233ce3df7ae2927632b626d7565a9
Horea Christian <horea.christ@yandex.com>
2017-05-29 05:07:28 +0200

    typo

--- labbookdb/cli.py
+++ labbookdb/cli.py
@@ -2,7 +2,7 @@ __author__ = "Horea Christian"
 
 import argh
 
-#the order here is very important, if these modules are called in the wrong order, the declarative based will end up being regenerated and will complain about duplicates along the lines of:
+#the order here is very important, if these modules are called in the wrong order, the declarative base will end up being regenerated and will complain about duplicates along the lines of:
 #sqlalchemy.exc.InvalidRequestError: Table 'genotype_associations' is already defined for this MetaData instance.  Specify 'extend_existing=True' to redefine options and columns on an existing Table object.
 try:
 	from db.query import animal_info, cage_info
