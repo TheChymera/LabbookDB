297d7451a0280177cde19173f6034fbb479113aa
Horea Christian <horea.christ@yandex.com>
2017-05-30 11:07:49 +0200

    supporting linker tables

--- labbookdb/introspection/schema.py
+++ labbookdb/introspection/schema.py
@@ -3,20 +3,27 @@ import pydotplus
 import sadisplay
 from os import path
 from labbookdb.db.query import ALLOWED_CLASSES
+from labbookdb.db.common_classes import *
 
 def generate(
 	extent="all",
 	save_dotfile="",
 	save_plot="",
 	label="",
+	linker_tables=False
 	):
 	"""Retreive the LabbookDB schema and save either a DOT file, or a PNG or PDF plot.
 	"""
 
 	if extent == "all":
-		desc = sadisplay.describe(ALLOWED_CLASSES.values())
+		nodes = ALLOWED_CLASSES.values()
 	elif type(extent) is list:
-		desc = sadisplay.describe([ALLOWED_CLASSES[key] for key in extent])
+		nodes = [ALLOWED_CLASSES[key] for key in extent]
+
+	if linker_tables:
+		nodes.extend(linker_tables)
+
+	desc = sadisplay.describe(nodes)
 
 	if save_dotfile:
 		save_dotfile = path.abspath(path.expanduser(save_dotfile))
@@ -36,5 +43,25 @@ def generate(
 
 if __name__ == '__main__':
 	# generate(extent="all", save_plot="~/full_schema.png")
-	generate(extent=["Animal","CageStay","Cage","Genotype","SucrosePreferenceMeasurement","FMRIMeasurement"], save_plot="~/measurements_schema.pdf")
-	# generate(extent=["Animal","CageStay","Cage"], save_plot="~/cagestay_schema.png")
+	# generate(extent=["Animal","CageStay","Cage","OpticFiberImplantProtocol","Operation","OrthogonalStereotacticTarget","Protocol"], save_plot="~/cagestay_schema.pdf", linker_tables=[operation_association])
+	# generate(extent=["Animal","CageStay","Cage",], save_plot="~/cagestay_schema.pdf", linker_tables=[cage_stay_association])
+	generate(
+		extent=[
+			"Animal",
+			"ForcedSwimTestMeasurement",
+			"Evaluation",
+			"CageStay",
+			"Cage",
+			"Measurement",
+			"Treatment",
+			"TreatmentProtocol",
+			"Protocol",
+			],
+		save_plot="~/fst_schema.pdf",
+		linker_tables=[
+			cage_stay_association,
+			treatment_cage_association,
+			],
+		)
+	# generate(extent=["Animal","CageStay","Cage","SucrosePreferenceMeasurement"], save_plot="~/measurements_schema.pdf")
+	# generate(extent=["Animal","FMRIMeasurement","OpenFieldTestMeasurement","WeightMeasurement"], save_plot="~/measurements_schema.pdf")
