0def422d66cfabeda7281ef711f4f292bf78f01a
Horea Christian <horea.christ@yandex.com>
2017-05-30 05:07:40 +0200

    corrected typo

--- labbookdb/report/tracking.py
+++ labbookdb/report/tracking.py
@@ -87,8 +87,8 @@ def animals_info(db_path,
 	df['nonresponsive'] = nonresponder_df
 	df['functional'] = functional_scan_df
 	df[['nonresponsive', 'functional']] = df[["nonresponsive", 'functional']].fillna(0).astype(int)
-	df["responsive functional meausrements"] = df['functional'] - df['nonresponsive']
-	df["responsive functional meausrements"] = df["responsive functional meausrements"].astype(str) +"/"+ df['functional'].astype(str)
+	df["responsive functional measurements"] = df['functional'] - df['nonresponsive']
+	df["responsive functional measurements"] = df["responsive functional measurements"].astype(str) +"/"+ df['functional'].astype(str)
 	df.drop(['nonresponsive', 'functional'], axis = 1, inplace = True, errors = 'ignore')
 	df = df.sort_index(ascending=False)
 
