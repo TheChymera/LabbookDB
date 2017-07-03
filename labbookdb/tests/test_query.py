e29a57c5e20c29c3ef02d95feba4fd928795624d
Horea Christian <horea.christ@yandex.com>
2017-07-03 18:42:11 +0200

    corrected exception name

--- labbookdb/tests/test_query.py
+++ labbookdb/tests/test_query.py
@@ -4,9 +4,9 @@ from labbookdb.db import add
 
 def test_related_entry_separators():
 	session, engine = add.load_session("~/somepath.db")
-	with pytest.raises(Exception) as excinfo:
+	with pytest.raises(BaseException) as excinfo:
 		add.get_related_ids(session, engine, "Animal:external_ids.AnimalExternalIdentifier:database.ETH/AIC/cdb&#&identifier.275511")
 	assert excinfo.value.args[0] == 'No entry was found with a value of "275511" on the "identifier" column of the "AnimalExternalIdentifier" CATEGORY, in the database.'
-	with pytest.raises(Exception) as excinfo:
+	with pytest.raises(BaseException) as excinfo:
 		add.get_related_ids(session, engine, "Animal:external_ids.AnimalExternalIdentifier:database.ETH/AIC/cdb&&identifier.275511")
 	assert excinfo.value.args[0] == 'No entry was found with a value of "ETH/AIC/cdb" on the "database" column of the "AnimalExternalIdentifier" CATEGORY, in the database.'
