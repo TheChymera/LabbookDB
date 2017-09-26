35b20bcd18fd3fd012ac44047d6e81d0eb4541e8
Horea Christian <horea.christ@yandex.com>
2017-09-26 03:22:43 +0200

    Testing with demolog data

--- labbookdb/report/development.py
+++ labbookdb/report/development.py
@@ -46,7 +46,7 @@ def groups():
 	"""Create a `pandas.DataFrame` containing treatment and genotype group assignments"""
 	from labbookdb.report.tracking import treatment_group, append_external_identifiers
 
-	db_path = '~/syncdata/meta.db'
+	db_path = '~/src/demolog/meta.db'
 
 	df = treatment_group(db_path, ['cFluDW','cFluDW_'], 'cage')
 	df = append_external_identifiers(db_path, df, ['Genotype_code'])
