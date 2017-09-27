900431e3f89108e4289be93db54f39210f4b0ea2
Horea Christian <horea.christ@yandex.com>
2017-09-27 04:21:18 +0200

    Checking outputs

--- labbookdb/tests/test_report.py
+++ labbookdb/tests/test_report.py
@@ -4,7 +4,11 @@ def test_groups():
 	"""Create a `pandas.DataFrame` containing treatment and genotype group assignments"""
 	from labbookdb.report.tracking import treatment_group, append_external_identifiers
 
-	db_path = '~/syncdata/meta.db'
+	known_sorted_ids = ['5667', '5668', '5673', '5674', '5675', '5689', '5690', '5691', '5692', '5693', '5694', '5694', '5699', '5700', '5704', '5705', '5706', '6254', '6255', '6256', '6262']
 
+	db_path = '~/src/demolog/from_python_code/meta.db'
 	df = treatment_group(db_path, ['cFluDW','cFluDW_'], 'cage')
 	df = append_external_identifiers(db_path, df, ['Genotype_code'])
+	sorted_ids = sorted(df['ETH/AIC'].tolist())
+
+	assert sorted_ids == known_sorted_ids
