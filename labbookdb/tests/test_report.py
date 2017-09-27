6b55e0bc1851917388383105ccb117771bcbbbe5
Horea Christian <horea.christ@yandex.com>
2017-09-27 02:23:13 +0200

    Tabs not spaces

--- labbookdb/tests/test_report.py
+++ labbookdb/tests/test_report.py
@@ -1,10 +1,10 @@
 import pytest
 
 def test_groups():
-        """Create a `pandas.DataFrame` containing treatment and genotype group assignments"""
-        from labbookdb.report.tracking import treatment_group, append_external_identifiers
+	"""Create a `pandas.DataFrame` containing treatment and genotype group assignments"""
+	from labbookdb.report.tracking import treatment_group, append_external_identifiers
 
-        db_path = '~/syncdata/meta.db'
+	db_path = '~/syncdata/meta.db'
 
-        df = treatment_group(db_path, ['cFluDW','cFluDW_'], 'cage')
-        df = append_external_identifiers(db_path, df, ['Genotype_code'])
+	df = treatment_group(db_path, ['cFluDW','cFluDW_'], 'cage')
+	df = append_external_identifiers(db_path, df, ['Genotype_code'])
