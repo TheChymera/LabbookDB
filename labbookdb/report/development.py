98a565e5053a692d7c6302c85dca58ed6f5424c0
Horea Christian <horea.christ@yandex.com>
2019-06-16 07:04:55 +0200

    Improved filter parametrization

--- labbookdb/report/development.py
+++ labbookdb/report/development.py
@@ -1,23 +1,41 @@
 
 def animal_multiselect(db_path,
-	cage_treatments=['cFluDW','cFluDW_'],
+	cage_treatments=[],
 	implant_targets=['dr_impl'],
 	virus_targets=['dr_skull','dr_dura','dr_dura_shallow','dr_skull_perpendicular'],
-	genotype_targets=['eptg'],
+	genotypes=['eptg'],
 	external_id='',
 	):
 	from labbookdb.report.selection import animals_by_genotype, animal_id, animal_treatments, animal_operations
 
-	df_treatments = animal_treatments(db_path, cage_treatments=cage_treatments)['Animal_id'].tolist()
-	df_implants = animal_operations(db_path, implant_targets=implant_targets)['Animal_id'].tolist()
-	df_virus = animal_operations(db_path, virus_targets=virus_targets)['Animal_id'].tolist()
-	genotype = animals_by_genotype(db_path, genotype_targets)['Animal_id'].tolist()
+	if implant_targets:
+		df_implants = animal_operations(db_path, implant_targets=implant_targets)['Animal_id'].tolist()
+	if virus_targets:
+		df_virus = animal_operations(db_path, virus_targets=virus_targets)['Animal_id'].tolist()
+	if genotypes:
+		df_genotype = animals_by_genotype(db_path, genotypes)['Animal_id'].tolist()
+	if cage_treatments:
+		df_treatments = animal_treatments(db_path, cage_treatments=cage_treatments)['Animal_id'].tolist()
+
 	if external_id:
-		df_treatments = [animal_id(db_path, external_id, i, reverse=True) for i in df_treatments]
-		df_implants = [animal_id(db_path, external_id, i, reverse=True) for i in df_implants]
-		df_virus = [animal_id(db_path, external_id, i, reverse=True) for i in df_virus]
-		genotype = [animal_id(db_path, external_id, i, reverse=True) for i in genotype]
-	d = [df_treatments, df_implants, df_virus, genotype]
+		if implant_targets:
+			df_implants = [animal_id(db_path, external_id, i, reverse=True) for i in df_implants]
+		if virus_targets:
+			df_virus = [animal_id(db_path, external_id, i, reverse=True) for i in df_virus]
+		if genotypes:
+			df_genotype = [animal_id(db_path, external_id, i, reverse=True) for i in df_genotype]
+		if cage_treatments:
+			df_treatments = [animal_id(db_path, external_id, i, reverse=True) for i in df_treatments]
+
+	d = []
+	if implant_targets:
+		d.append(df_implants)
+	if virus_targets:
+		d.append(df_virus)
+	if genotypes:
+		d.append(df_genotype)
+	if cage_treatments:
+		d.append(df_treatments)
 	selection = list(set(d[0]).intersection(*d))
 	return selection
 
