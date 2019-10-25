11c5e0f6043073337aed4a9a2169c29de406949d
Horea Christian <chr@chymera.eu>
2019-10-25 18:08:28 +0200

    No longer polluting homedir

--- labbookdb/tests/test_query.py
+++ labbookdb/tests/test_query.py
@@ -3,7 +3,7 @@ import pytest
 from labbookdb.db import add
 
 def test_related_entry_separators():
-	session, engine = add.load_session("~/somepath.db")
+	session, engine = add.load_session("/tmp/somepath.db")
 	with pytest.raises(BaseException) as excinfo:
 		add.get_related_ids(session, engine, "Animal:external_ids.AnimalExternalIdentifier:database.ETH/AIC/cdb&#&identifier.275511")
 	assert excinfo.value.args[0] == 'No entry was found with a value of "275511" on the "identifier" column of the "AnimalExternalIdentifier" CATEGORY, in the database.'
